9930aa0c639a6f971d68331ff5cdc92c4bb889c8
Ashish Mool <asis.mool@gmail.com>
2023-12-05 14:54:51 +0545

    Homepage: Header
    -- Header elements added
    -- Css modified

--- abroad_dreams_frontend/src/pages/Home.tsx
+++ abroad_dreams_frontend/src/pages/Home.tsx
@@ -2,9 +2,29 @@ import "../css/Home.css";
 
 function Home(){
     return (
+        <div>
+            <div className={"header"}>
+                <div className={"logo"}>
+                    <img src={"src/assets/logo/ad-square-logo.svg"}/>
+                </div>
 
-        <>
-        </>
+                <div className={"links"}>
+                    <a href={"#"}>Home</a>
+                    <a href={"#"}>Services</a>
+                    <a href={"#"}>Who We Are</a>
+                    <a href={"#"}>Contact Us</a>
+                </div>
+
+                <div className={"login-register"}>
+                        <input type={"email"} alt={"Email"} placeholder={"Enter Email Addess"}/>
+                        <input type={"password"} alt={"Password"} placeholder={"Enter Password"}/>
+                        <button type={"submit"} className="btn-login" name={"Login"}>Login</button>
+                        <button type={"submit"} className="btn-register" name={"Register"}>Register</button>
+                </div>
+
+            </div>
+        </div>
+
 
     )
 }
