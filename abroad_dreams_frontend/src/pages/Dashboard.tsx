72cd9b8c66b370ef9bee7d28693b710753a96e10
Karanbohara01 <karanbohara216@gmail.com>
2023-12-06 20:37:15 +0545

    some components on header are created
    --my colleges' section
    --financial aid section
    --Common App section

--- abroad_dreams_frontend/src/pages/Dashboard.tsx
+++ abroad_dreams_frontend/src/pages/Dashboard.tsx
@@ -1,41 +1,40 @@
+// Importing the CSS file
 import "../css/Dashboard.css";
 
-
+// Dashboard component
 function Dashboard() {
-  
-
-return(
-
-  <div className="header">
-
-    <div className="left-section">
-    <img className="dream-logo" src="src/assets/logo/ad-square-logo.svg"/>
-      <img className="ham-logo" src="src/assets/home-slideshow/Hamburger_icon.png" alt="" />
-      <div className="left-container">
+  return (
+    <div className="header">
+      {/* Left Section */}
+      <div className="left-section">
+        <img className="dream-logo" src="src/assets/logo/ad-square-logo.svg" alt="Dream Logo" />
+        <img className="ham-logo" src="src/assets/home-slideshow/Hamburger_icon.png" alt="Hamburger Logo" />
+        <div className="left-container">
+          {/* Add content for the left container if needed */}
+        </div>
       </div>
+
+      {/* Middle Section */}
+      <div className="middle-section">
+        <input className="search-bar-section" placeholder="College Search" type="text" />
+        <ul>
+          <li>My Colleges</li>
+          <li>Common App</li>
+          <li>Financial Aids</li>
+        </ul>
+      </div>
+
+      {/* Right Section */}
+      <div className="right-section">
+        <img className="notification-logo" src="src/assets/home-slideshow/notification-icon.png" alt="Notification Logo" />
+        <img className="profile-image" src="src/assets/home-slideshow/profile.jpg" alt="Profile Image" />
+        <div className="admin-info">
+          <h3>Karan Bohara</h3>
+        </div>
+      </div>
+  
     </div>
-    <div className="middle-section">
-      <input className="search-bar-section" placeholder="Search  for" type="text" /></div>
-    <div className="right-section">
-
-    <img className="notification-logo" src="src/assets/home-slideshow/notification-icon.png" alt="no img" />
-
-    <img className="profile-image" src="src/assets/home-slideshow/profile.jpg" alt="no img" />
-
-    <div className="admin-info">
-      <h3>Karan Bohara</h3>
-    </div>
-
-
-    
-    </div>
-  </div>
-
-
-
-)
-};
-
-
+  );
+}
 
 export default Dashboard;
