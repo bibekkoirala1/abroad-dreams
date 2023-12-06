f522c78554476dbeca281137130d4e2802d12fc9
Karanbohara01 <karanbohara216@gmail.com>
2023-12-06 08:32:48 +0545

    dashboard layout
    -- css update
    -- tsx update

--- abroad_dreams_frontend/src/pages/Dashboard.tsx
+++ abroad_dreams_frontend/src/pages/Dashboard.tsx
@@ -2,12 +2,47 @@ import "../css/Dashboard.css";
 
 
 function Dashboard() {
-  return (
-    <div>
+  
 
+return(
+
+  <div
+   className="container">
+    <div
+     className="dashboard-header">
+      <h1>Dashboard header</h1>
     </div>
+    <div
+     className="dashboard-main">
 
-  )
-}
+      <div className="side-bar">
+        <p>left</p>
+        <p>left</p>
+        <p>left</p>
+        <p>left</p>
+        <p>left</p>
+        <p>left</p>
+        <p>left</p>
+        <p>left</p>
+        <p>left</p>
+      </div>
+
+      <div className="main-body">
+        <p>right</p>
+      </div>
+
+      <h1>Main body</h1>
+
+      </div>
+       <div className="dashboard-footer">
+      <h1>Dashboard Footer</h1>
+    </div>
+  </div>
+ 
+
+)
+};
+
+
 
 export default Dashboard;
