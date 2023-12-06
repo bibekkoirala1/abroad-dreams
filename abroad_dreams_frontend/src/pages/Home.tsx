7b913bef3eff304f9fd7fb036d5bb2c973048a5d
Ashish Mool <asis.mool@gmail.com>
2023-12-06 20:57:02 +0545

    Homepage: Course Finder
    -- added course finder feature
    -- css updated

--- abroad_dreams_frontend/src/pages/Home.tsx
+++ abroad_dreams_frontend/src/pages/Home.tsx
@@ -44,19 +44,49 @@ function Home(){
             </div>
 
             <div className="main-body">
-            <div className="slider-frame">
-            <div className="slide-images">
-                    <div className="img-container">
-                        <img src="src/assets/home-slideshow/slideshow-1.jpg"/>
+                <div className="course-finder">
+
+                    <div className="search-container">
+                        <input type="search" alt="course-search" placeholder="Search Available Courses" />
+
+                        <select name="duration" id="duration">
+                            <option value="">Select Duration</option>
+                            <option value="short">1-2 Years</option>
+                            <option value="medium">3 Years</option>
+                            <option value="long">4 Years</option>
+                        </select>
+
+                        <select name="country" id="country">
+                            <option value="">Select Country</option>
+                            <option value="usa">United States</option>
+                            <option value="uk">United Kingdom</option>
+                            <option value="india">India</option>
+                        </select>
+
+                        <select name="subject" id="subject">
+                            <option value="">Select Subject</option>
+                            <option value="math">Mathematics</option>
+                            <option value="science">Science</option>
+                            <option value="history">History</option>
+                        </select>
+
+                        <button type="submit" className="btn-search" name="search">Search</button>
                     </div>
-                    <div className="img-container">
-                    <img src="src/assets/home-slideshow/slideshow-2.jpg"/>
+                </div>
+
+                <div className="slider-frame">
+                    <div className="slide-images">
+                            <div className="img-container">
+                                <img src="src/assets/home-slideshow/slideshow-1.jpg"/>
+                            </div>
+                            <div className="img-container">
+                            <img src="src/assets/home-slideshow/slideshow-2.jpg"/>
+                            </div>
+                            <div className="img-container">
+                            <img src="src/assets/home-slideshow/slideshow-3.jpg"/>
+                            </div>
                     </div>
-                    <div className="img-container">
-                    <img src="src/assets/home-slideshow/slideshow-3.jpg"/>
-                    </div>
-            </div>
-        </div>
+                </div>
             </div>
 
 
